96f18135f627bd5089fb1f9d8684146e3c88961b
Taufiq <taufiqhaikal002@gmail.com>
2026-02-10 01:50:15 +0800

    fixed install procedure

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p vim git --run "bash ./install.sh"
 
+# Clone necessary repos
 git clone https://github.com/unlucky-shen/.FlakeBtw.git
 git clone https://github.com/unlucky-shen/hypr.git
 git clone https://github.com/unlucky-shen/kitty.git
-sudo mv .FlakeBtw/install/configuration.nix /etc/nixos/configuration.nix
-sudo nixos-rebuild switch
+
+# Copy generated hardware config to flake directory
+sudo cp /etc/nixos/hardware-configuration.nix /home/tau/.FlakeBtw
+
+# Apply config
+cd .FlakeBtw
+sudo nixos-rebuild switch --flake .#Tau --impure
+
--- system/default.nix
+++ system/default.nix
@@ -2,7 +2,7 @@
 
 {
 	imports = [ 
-		/etc/nixos/hardware-configuration.nix 
+		./hardware-configuration.nix 
 		./nvidia.nix
 		./packages.nix
 		./services.nix

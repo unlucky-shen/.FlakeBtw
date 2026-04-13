885744df1700ce34abb4cc52ce639bf2d7039a5b
Taufiq Haikal <taufiqhaikal002@gmail.com>
2026-04-14 01:17:58 +0800

    Manual list creation

--- system/wm.nix
+++ system/wm.nix
@@ -4,6 +4,9 @@
 	# wayland tweaks
 	environment.sessionVariables = {
 		NIXOS_OZONE_WL = "1";
+		XDG_CURRENT_DESKTOP = "Hyprland";
+		XDG_SESSION_TYPE = "wayland";
+		XDG_SESSION_DESKTOP = "Hyprland";
 	};
 
 	xdg.portal = {
--- system/xdg.nix
+++ system/xdg.nix
@@ -3,29 +3,34 @@
 {
 	xdg.portal = {
 		enable = true;
-		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+		extraPortals = [ 
+			pkgs.xdg-desktop-portal-gtk 
+			pkgs.xdg-desktop-portal-hyprland
+		];
 		config.common.default = "*";
 	};
 
-	xdg.mime = {
-		enable = true;
-		addedAssociations = {
-			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "libreoffice-writer.desktop" ];
-			"application/msword" = [ "libreoffice-writer.desktop" ];
-			"application/vnd.oasis.opendocument.text" = [ "libreoffice-writer.desktop" ];
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "libreoffice-calc.desktop" ];
-			"application/vnd.ms-excel" = [ "libreoffice-calc.desktop" ];
-			"application/vnd.oasis.opendocument.spreadsheet" = [ "libreoffice-calc.desktop" ];
-			"application/pdf" = [ "org.pwmt.zathura.desktop" ];
-		};
-		defaultApplications = {
-			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "libreoffice-writer.desktop" ];
-			"application/msword" = [ "libreoffice-writer.desktop" ];
-			"application/vnd.oasis.opendocument.text" = [ "libreoffice-writer.desktop" ];
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "libreoffice-calc.desktop" ];
-			"application/vnd.ms-excel" = [ "libreoffice-calc.desktop" ];
-			"application/vnd.oasis.opendocument.spreadsheet" = [ "libreoffice-calc.desktop" ];
-			"application/pdf" = [ "org.pwmt.zathura.desktop" ];
-		};
-	};
+	# xdg.mime = {
+	# 	enable = true;
+	# 	defaultApplications = {
+	# 		"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "libreoffice-writer.desktop";
+	# 		"application/msword" = "libreoffice-writer.desktop";
+	# 		"application/vnd.oasis.opendocument.text" = "libreoffice-writer.desktop";
+	# 		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "libreoffice-calc.desktop";
+	# 		"application/vnd.ms-excel" = "libreoffice-calc.desktop";
+	# 		"application/vnd.oasis.opendocument.spreadsheet" = "libreoffice-calc.desktop";
+	# 		"application/pdf" = "org.pwmt.zathura.desktop";
+	# 	};
+	# };
+
+	environment.etc."xdg/mimeapps.list".text = ''
+		[Default Applications]
+		application/vnd.openxmlformats-officedocument.wordprocessingml.document=libreoffice-writer.desktop;
+		application/msword=libreoffice-writer.desktop;
+		application/vnd.oasis.opendocument.text=libreoffice-writer.desktop;
+		application/vnd.openxmlformats-officedocument.spreadsheetml.sheet=libreoffice-calc.desktop;
+		application/vnd.ms-excel=libreoffice-calc.desktop;
+		application/vnd.oasis.opendocument.spreadsheet=libreoffice-calc.desktop;
+		application/pdf=org.pwmt.zathura.desktop;
+	'';
 }

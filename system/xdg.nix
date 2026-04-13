c1ca12be0fbb473ff20a99519f3beb9088cfd92c
Taufiq Haikal <taufiqhaikal002@gmail.com>
2026-04-14 01:31:21 +0800

    used xdg.mime instead

--- system/xdg.nix
+++ system/xdg.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
 	xdg.portal = {
@@ -10,27 +10,20 @@
 		config.common.default = "*";
 	};
 
-	# xdg.mime = {
-	# 	enable = true;
-	# 	defaultApplications = {
-	# 		"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "libreoffice-writer.desktop";
-	# 		"application/msword" = "libreoffice-writer.desktop";
-	# 		"application/vnd.oasis.opendocument.text" = "libreoffice-writer.desktop";
-	# 		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "libreoffice-calc.desktop";
-	# 		"application/vnd.ms-excel" = "libreoffice-calc.desktop";
-	# 		"application/vnd.oasis.opendocument.spreadsheet" = "libreoffice-calc.desktop";
-	# 		"application/pdf" = "org.pwmt.zathura.desktop";
-	# 	};
-	# };
-
-	environment.etc."xdg/mimeapps.list".text = ''
-		[Default Applications]
-		application/vnd.openxmlformats-officedocument.wordprocessingml.document=libreoffice-writer.desktop;
-		application/msword=libreoffice-writer.desktop;
-		application/vnd.oasis.opendocument.text=libreoffice-writer.desktop;
-		application/vnd.openxmlformats-officedocument.spreadsheetml.sheet=libreoffice-calc.desktop;
-		application/vnd.ms-excel=libreoffice-calc.desktop;
-		application/vnd.oasis.opendocument.spreadsheet=libreoffice-calc.desktop;
-		application/pdf=org.pwmt.zathura.desktop;
-	'';
+	xdg.mime = let
+		writer = "libreoffice-writer.desktop";
+		calc = "libreoffice-calc.desktop";
+		zathura = "org.pwmt.zathura.desktop";
+	in {
+		enable = true;
+		defaultApplications = {
+			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = writer;
+			"application/msword" = writer;
+			"application/vnd.oasis.opendocument.text" = writer;
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = calc;
+			"application/vnd.ms-excel" = calc;
+			"application/vnd.oasis.opendocument.spreadsheet" = calc;
+			"application/pdf" = zathura;
+		};
+	};
 }

49d6a365c16e6a10a4f225b405f634bc883fae2b
Taufiq Haikal <taufiqhaikal002@gmail.com>
2026-04-13 17:41:00 +0800

    Fixed some errors

--- system/xdg.nix
+++ system/xdg.nix
@@ -1,25 +1,27 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
 	xdg.mime = {
 		enable = true;
+		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+		config.common.default = "*";
 		addedAssociations = {
-			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "libreoffice-writer.desktop";
-			"application/msword" = "libreoffice-writer.desktop";
-			"application/vnd.oasis.opendocument.text" = "libreoffice-writer.desktop";
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "libreoffice-calc.desktop";
-			"application/vnd.ms-excel" = "libreoffice-calc.desktop";
-			"application/vnd.oasis.opendocument.spreadsheet" = "libreoffice-calc.desktop";
-			"application/pdf" = "org.pwmt.zathura.desktop";
+			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "libreoffice-writer.desktop" ];
+			"application/msword" = [ "libreoffice-writer.desktop" ];
+			"application/vnd.oasis.opendocument.text" = [ "libreoffice-writer.desktop" ];
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "libreoffice-calc.desktop" ];
+			"application/vnd.ms-excel" = [ "libreoffice-calc.desktop" ];
+			"application/vnd.oasis.opendocument.spreadsheet" = [ "libreoffice-calc.desktop" ];
+			"application/pdf" = [ "org.pwmt.zathura.desktop" ];
 		};
 		defaultApplications = {
-			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "libreoffice-writer.desktop";
-			"application/msword" = "libreoffice-writer.desktop";
-			"application/vnd.oasis.opendocument.text" = "libreoffice-writer.desktop";
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "libreoffice-calc.desktop";
-			"application/vnd.ms-excel" = "libreoffice-calc.desktop";
-			"application/vnd.oasis.opendocument.spreadsheet" = "libreoffice-calc.desktop";
-			"application/pdf" = "org.pwmt.zathura.desktop";
+			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "libreoffice-writer.desktop" ];
+			"application/msword" = [ "libreoffice-writer.desktop" ];
+			"application/vnd.oasis.opendocument.text" = [ "libreoffice-writer.desktop" ];
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "libreoffice-calc.desktop" ];
+			"application/vnd.ms-excel" = [ "libreoffice-calc.desktop" ];
+			"application/vnd.oasis.opendocument.spreadsheet" = [ "libreoffice-calc.desktop" ];
+			"application/pdf" = [ "org.pwmt.zathura.desktop" ];
 		};
 	};
 }

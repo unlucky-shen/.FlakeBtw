d48c53e9670b7840e068841459dcdab98792e010
Taufiq Haikal <taufiqhaikal002@gmail.com>
2026-04-16 17:54:16 +0800

    Added QoL Plugins

--- nixvim/plugins/default.nix
+++ nixvim/plugins/default.nix
@@ -5,7 +5,9 @@
 		web-devicons.enable = true;
 		mini-pick.enable = true;
 		nvim-autopairs.enable = true;
+		flash.enable = true;
 		indent-blankline.enable = true;
+		friendly-snippets.enable = true;
 		luasnip.enable = true;
 		lualine.enable = true;
 	};
